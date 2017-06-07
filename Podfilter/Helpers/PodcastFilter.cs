e96dfabdfddb9f00427d42a3405f2259df4a025c
j.freese <freese.johannes@gmail.com>
2017-06-07 19:05:37 +0200

    Added namespaces to PodcastFilter.

--- Podfilter/Helpers/PodcastFilter.cs
+++ Podfilter/Helpers/PodcastFilter.cs
@@ -7,6 +7,9 @@ namespace Podfilter.Helpers
     /// </summary>
     public class PodcastFilter : IFiltersPodcasts
     {
+        private static XNamespace _itunesNamespace = "http://www.itunes.com/dtds/podcast-1.0.dtd";
+        private static XNamespace _atomNamespace = "http://www.w3.org/2005/Atom";
+        
         public XDocument FilterPodcast(XDocument podcast)
         {
             throw new System.NotImplementedException();
