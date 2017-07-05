f66cee79fa8c68cb87de6fe56773bf58ae7b7bc1
j.freese <freese.johannes@gmail.com>
2017-07-05 21:58:30 +0200

    Started work on the PidcastAction base class.

---
+++ Podfilter/Models/PodcastActions/PodcastAction.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Linq;
+using Podfilter.Models.ContentActions;
+
+namespace Podfilter.Models.PodcastActions
+{
+    public abstract class PodcastAction : IPodcastAction
+    {
+        private List<IContentAction> Actions { get; set; } = new List<IPodcastAction>();
+        
+        public XDocument PerformAction(XDocument podcast)
+        {
+            
+        }
+
+        public abstract IEnumerable<XElement> GetMatchingElements();
+    }
+}
