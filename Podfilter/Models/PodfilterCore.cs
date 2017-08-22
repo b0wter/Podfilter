f1197ec08deda51badb059a8c4fcb847f9ab5c74
b0wter <evil-klabautermann@gmx.de>
2017-08-22 13:52:27 +0200

    Added a first version of the web layout.

---
+++ Podfilter/Models/PodfilterCore.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Podfilter.Models
+{
+    public class PodfilterCore
+    {
+    }
+}
--- Podfilter/wwwroot/js/site.js
+++ Podfilter/wwwroot/js/site.js
@@ -1,1 +1,8 @@
-﻿// Write your Javascript code.
+﻿// Open/close overlay for new filters.
+//
+function openNewFilterOverlay() {
+    document.getElementById("newFilterOverlay").style.width = "100%";
+}
+function openNewFilterOverlay() {
+    document.getElementById("newFilterOverlay").style.width = "0%";
+}
