cf78d41978f07e9a77c2beaf7015da93247b0b2f
b0wter <evil-klabautermann@gmx.de>
2017-08-24 23:40:38 +0200

    Removed Angular project and continued work on "regular" web frontend.

--- PodfilterWeb/Controllers/HomeController.cs
+++ PodfilterWeb/Controllers/HomeController.cs
@@ -3,17 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using PodfilterCore.Models.PodcastModification;
 
 namespace PodfilterWeb.Controllers
 {
     public class HomeController : HtmlBaseController
     {
+        private List<BasePodcastModification> _currentModifications = new List<BasePodcastModification>();
+
         /// <summary>
         /// Index view displays the main ui for creating new podcast urls.
         /// </summary>
         /// <returns></returns>
         public IActionResult Index()
         {
+            return View();
+        }
+
+        [HttpPost("/addFilter")]
+        public IActionResult AddFilter([FromForm] string filterType)
+        {
+            // in case the select box hint was submitted
+            if(string.IsNullOrWhiteSpace(filterType))
+                return View();
+
+
+
             return View();
         }
 
