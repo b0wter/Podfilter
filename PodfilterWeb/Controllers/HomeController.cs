4851dab5334fac8441c0f88432f490a8a05be737
b0wter <evil-klabautermann@gmx.de>
2017-08-29 16:28:25 +0200

    Better encapsulation of the session content. Moved parameter box html to a new file. Input boxes for podcast urls are now 100% width. Layout makes better use of larger screens.

--- PodfilterWeb/Controllers/HomeController.cs
+++ PodfilterWeb/Controllers/HomeController.cs
@@ -9,12 +9,16 @@ using PodfilterWeb.Models;
 using System.Net.Http;
 using System.Xml.Linq;
 using System.Xml;
+using System.Collections.ObjectModel;
 
 namespace PodfilterWeb.Controllers
 {
     public class HomeController : HtmlBaseController
     {
         private const string PodcastUrlKey = "podcastUrl";
+        /// <summary>
+        /// Property encapsulating the contents of the "podcastUrl"-value in the current session.
+        /// </summary>
         private string PodcastUrl
         {
             get
@@ -36,8 +40,54 @@ namespace PodfilterWeb.Controllers
             }
         }
 
+        // Handles the session management of the modifications. Cannot be implemented as a property because of the constant (de)serialization which breaks the handlers.
         private const string PodcastModificationsKey = "podcastModifications";
+        private void SetPodcastModifications(List<DisplayableBasePodcastModification> mods)
+        {
+            if (HttpContext.Session.IsAvailable)
+            {
+                HttpContext.Session.Set<List<DisplayableBasePodcastModification>>(PodcastModificationsKey, mods);
+            }
+        }
+        private List<DisplayableBasePodcastModification> GetPodcastModifications()
+        {
+            if(HttpContext.Session.IsAvailable && HttpContext.Session.Keys.Contains(PodcastModificationsKey))
+            {
+                // Since the list is deserialized it never returns null, only an empty list.
+                var cachedModifications = HttpContext.Session.Get<List<DisplayableBasePodcastModification>>(PodcastModificationsKey);
+                return cachedModifications;
+            }
+            else
+            {
+                return new List<DisplayableBasePodcastModification>();
+            }
+        }
+        
         private const string SelectedFilterKey = "selectedFilter";
+        /// <summary>
+        /// Encapsulates the contents of the "selectedFilter"-value in the current session.
+        /// </summary>
+        private int SelectedFilterIndex
+        {
+            get
+            {
+                if (HttpContext.Session.IsAvailable)
+                {
+                    var selection = HttpContext.Session.Get<int>(SelectedFilterKey);
+                    return selection;
+                }
+
+                return -1;
+            }
+            set
+            {
+                if (HttpContext.Session.IsAvailable)
+                {
+                    HttpContext.Session.Set<int>(SelectedFilterKey, value);
+                }
+            }
+        }
+
         private BaseModificationMethodTranslator _methodTranslator;
 
         public HomeController(BaseModificationMethodTranslator methodTranslator)
@@ -52,43 +102,24 @@ namespace PodfilterWeb.Controllers
         public IActionResult Index()
         {
             // store filter information
-            var filters = GetSessionModificationsFromCache();
+            var filters = GetPodcastModifications();
             ViewData["currentFilters"] = filters;
             ViewData["currentFilterCount"] = filters.Count;
 
-            ViewData["selectedFilter"] = GetSelectedFilterFromCache();
+            ViewData["selectedFilter"] = SelectedFilterIndex;
 
             ViewData["podcastUrl"] = PodcastUrl;
             return View();
         }
 
-        private List<DisplayableBasePodcastModification> GetSessionModificationsFromCache()
-        {
-            if (HttpContext.Session.IsAvailable)
-            {
-                var cachedModifications = HttpContext.Session.Get<List<DisplayableBasePodcastModification>>(PodcastModificationsKey);
-                if (cachedModifications == null || cachedModifications.Count == 0)
-                    return new List<DisplayableBasePodcastModification>();
-                else
-                    return cachedModifications;
-            }
-            else
-            {
-                return new List<DisplayableBasePodcastModification>();
-            }
-        }
-
-        private int GetSelectedFilterFromCache()
-        {
-            if(HttpContext.Session.IsAvailable)
-            {
-                var selection = HttpContext.Session.Get<int>(SelectedFilterKey);
-                return selection;
-            }
-
-            return -1;
-        }
-
+        /// <summary>
+        /// Is called from the website if the user wants to add a new filter.
+        /// </summary>
+        /// <param name="filterType"></param>
+        /// <param name="newFilterArgument"></param>
+        /// <param name="newFilterMethod"></param>
+        /// <param name="urlInputField"></param>
+        /// <returns></returns>
         [HttpPost("/addFilter")]
         public IActionResult AddFilter([FromForm] string filterType, [FromForm] string[] newFilterArgument, [FromForm] string[] newFilterMethod, [FromForm] string urlInputField)
         {
@@ -100,9 +131,9 @@ namespace PodfilterWeb.Controllers
                     var modificationArgument = GetArgumentFromArgumentsArray(newFilterArgument);
                     var filterMethod = GetFilterMethodFromArgumentsArray(newFilterArgument, newFilterMethod);
                     var modification = CreateModificationFromArguments(filterType, modificationArgument, filterMethod);
-                    var existingFilters = HttpContext.Session.Get<List<DisplayableBasePodcastModification>>(PodcastModificationsKey) ?? new List<DisplayableBasePodcastModification>();
-                    existingFilters.Add(modification);
-                    HttpContext.Session.Set<List<DisplayableBasePodcastModification>>(PodcastModificationsKey, existingFilters);
+                    var existingModifications = GetPodcastModifications();
+                    existingModifications.Add(modification);
+                    SetPodcastModifications(existingModifications);
                 }
                 else
                 {
@@ -117,18 +148,24 @@ namespace PodfilterWeb.Controllers
             return Redirect("/");
         }
 
+        /// <summary>
+        /// Is called from the website if the user wants to remove a filter.
+        /// </summary>
+        /// <param name="urlInputField"></param>
+        /// <returns></returns>
         [HttpPost("/removeFilter")]
         public IActionResult RemoveFilter([FromForm] string urlInputField)
         {
             PodcastUrl = urlInputField;
             if(HttpContext.Session.IsAvailable)
             {
-                int filterIndex = GetSelectedFilterFromCache();
-                var modifications = GetSessionModificationsFromCache();
+                int filterIndex = SelectedFilterIndex;
+
+                var modifications = GetPodcastModifications();
                 if(filterIndex >= 0 && filterIndex < modifications.Count)
                 {
                     modifications.RemoveAt(filterIndex);
-                    HttpContext.Session.Set<List<DisplayableBasePodcastModification>>(PodcastModificationsKey, modifications);
+                    SetPodcastModifications(modifications);
                 }
                 else
                 {
@@ -139,6 +176,11 @@ namespace PodfilterWeb.Controllers
             return Redirect("/");
         }
 
+        /// <summary>
+        /// Called from the website if the user selected another filter.
+        /// </summary>
+        /// <param name="filterIndex"></param>
+        /// <returns></returns>
         [HttpGet("/selectFilter/{filterIndex}")]
         public IActionResult SelectFilter(int filterIndex)
         {
@@ -149,6 +191,11 @@ namespace PodfilterWeb.Controllers
             return Redirect("/");
         }
 
+        /// <summary>
+        /// Called from the website if the user wants to create a new podcast url.
+        /// </summary>
+        /// <param name="urlInputField"></param>
+        /// <returns></returns>
         [HttpPost("/create")]
         public IActionResult CreatePodcastUrl([FromForm] string urlInputField)
         {
@@ -156,7 +203,7 @@ namespace PodfilterWeb.Controllers
             if(string.IsNullOrWhiteSpace(urlInputField))
                 TempData["errorMessage"] = "You need to enter a podcast url.";
 
-            var modifications = GetSessionModificationsFromCache();
+            var modifications = GetPodcastModifications();
             if (modifications.Count <= 0)
                 TempData["warningMessage"] = "You have not added any filters.";
             else
@@ -172,6 +219,11 @@ namespace PodfilterWeb.Controllers
             return Redirect("/");
         }
 
+        /// <summary>
+        /// Called from the website if the user wnats to validate a remote url returns a valid xml document.
+        /// </summary>
+        /// <param name="urlInputField"></param>
+        /// <returns></returns>
         [HttpPost("/validate")]
         public async Task<IActionResult> ValidatePodcastUrl([FromForm] string urlInputField)
         {
@@ -197,13 +249,23 @@ namespace PodfilterWeb.Controllers
             return Redirect("/");
         }
 
+        /// <summary>
+        /// Retrieves the remote content of an url and checks it it's a valid xml document.
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns></returns>
         private async Task ValidateRemoteUrlIsXmlDocument(string url)
         {
+            //TODO: Replace HttpClient with interface!
             var client = new HttpClient();
             var content = await client.GetStringAsync(url);
             var document = XDocument.Parse(content);
         }
 
+        /// <summary>
+        /// Renders the about page.
+        /// </summary>
+        /// <returns></returns>
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
@@ -211,6 +273,10 @@ namespace PodfilterWeb.Controllers
             return View();
         }
 
+        /// <summary>
+        /// Renders the contact page.
+        /// </summary>
+        /// <returns></returns>
         public IActionResult Contact()
         {
             ViewData["Message"] = "Your contact page.";
@@ -218,11 +284,22 @@ namespace PodfilterWeb.Controllers
             return View();
         }
 
+        /// <summary>
+        /// Renders a generic error page.
+        /// </summary>
+        /// <returns></returns>
         public IActionResult Error()
         {
             return View();
         }
 
+        /// <summary>
+        /// Dynamically creates an instance of a subclass of <see cref="DisplayableBasePodcastModification"/> using the given typename and arguments.
+        /// </summary>
+        /// <param name="filterType"></param>
+        /// <param name="argument"></param>
+        /// <param name="method"></param>
+        /// <returns></returns>
         private DisplayableBasePodcastModification CreateModificationFromArguments(string filterType, string argument, string method)
         {
             var typeAsString = $"PodfilterWeb.Models.Displayable{filterType}, PodfilterWeb";
@@ -230,6 +307,11 @@ namespace PodfilterWeb.Controllers
             return modification;
         }
 
+        /// <summary>
+        /// Returns the first non-null string from <paramref name="arguments"/>.
+        /// </summary>
+        /// <param name="arguments"></param>
+        /// <returns></returns>
         private string GetArgumentFromArgumentsArray(string[] arguments)
         {
             if(arguments.Count(s => !string.IsNullOrWhiteSpace(s)) != 1)
@@ -238,6 +320,12 @@ namespace PodfilterWeb.Controllers
             return arguments.First(argument => string.IsNullOrWhiteSpace(argument) == false);
         }
 
+        /// <summary>
+        /// Returns the element of <paramref name="methods"/> whose index matches that of the first non-null string in <paramref name="arguments"/>.
+        /// </summary>
+        /// <param name="arguments"></param>
+        /// <param name="methods"></param>
+        /// <returns></returns>
         private string GetFilterMethodFromArgumentsArray(string[] arguments, string[] methods)
         {
             int i = 0;
@@ -245,6 +333,10 @@ namespace PodfilterWeb.Controllers
             return method;
         }
 
+        /// <summary>
+        /// Retrieves the base url of this web application.
+        /// </summary>
+        /// <returns></returns>
         private string GetBaseUrl()
         {
             var request = HttpContext.Request;
---
+++ PodfilterWeb/Helpers/SessionStorageList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PodfilterWeb.Helpers
+{
+    public class SessionStorageList<T> : List<T>
+    {
+        new public void Add(T element)
+        {
+            
+        }
+    }
+}
